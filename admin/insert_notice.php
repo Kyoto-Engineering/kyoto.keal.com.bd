57b9da82235d89f901c87a407a3df7cebcbc390b
Protyasha-Saha <sahaprotyasha@gmail.com>
2018-04-10 16:46:37 +0600

    added

---
+++ admin/insert_notice.php
@@ -0,0 +1,58 @@
+<?php include "inc/header.php" ; ?>
+        <!-- end navbar top -->
+
+        <!-- navbar side -->
+<?php include "inc/sidebar.php" ; ?> 
+        <!-- end navbar side -->
+        <!--  page-wrapper -->
+          
+
+
+
+
+
+
+
+
+
+
+          <div id="page-wrapper">
+           
+                <!--end page header -->
+           
+                                    <br>
+                                    <h1>Input Notice</h1>
+                                    <form class="col-md-8" >
+                                        <div class="form-group input-group">
+                                            <label>Notice</label>
+                                                <input type="text" class="form-control"  placeholder="Enter Notice" >
+                                        </div>
+                                        <div class="form-group input-group">
+                                            <label>Date</label>
+                                                <input type="text" class="form-control"  placeholder="Posted Date" >
+                                        </div>
+
+
+
+                                        <div class="form-group input-group">
+                                            <label>Posted by</label>
+                                            <input type="text" class="form-control" placeholder="Enter Posted by">
+                                        </div>
+                                        <button class="submit" name="submit" id="submit"><h4>Submit</h4></button>
+                                        
+                                    </form>
+                                
+                          
+                       
+                  
+                     <!-- End Form Elements -->
+               
+         
+        
+        <!-- end page-wrapper -->
+
+
+    <!-- end wrapper -->
+
+    <!-- Core Scripts - Include with every page -->
+<? php include "inc/footer.php" ; ?>
