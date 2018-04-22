4fb1a89d2d4748b73d8fa7462c37859ff90f84f4
Arnab1500117 <arnab.r@keal.com.bd>
2018-04-22 11:40:28 +0600

    added

---
+++ classes/asignin.php
@@ -0,0 +1,53 @@
+<?php
+	  include_once ('../lib/session.php');
+     Session::checkLogin();
+
+	 include_once ('../lib/database.php');
+	 include_once ('../helpers/format.php');
+
+
+?>
+
+<?php
+/**
+* Adminlogin class
+*/
+class Adminuserlogin
+{
+	private $db;
+	private $fm;
+	public function __construct()
+	{
+		$this->db = new Database();
+		$this->fm = new Format();
+	}
+
+	public function HumanResource($email, $pass){
+
+		$adminUser = $this->fm->validation($email);
+		$adminPass = $this->fm->validation($pass);
+
+		$adminUser = mysqli_real_escape_string($this->db->link, $adminUser);
+		$adminPass = mysqli_real_escape_string($this->db->link, $adminPass);
+
+		if (empty($adminUser) || empty($adminPass)) {
+			$logmsg = "Username Or Password Must Not be Empty!!";
+			return $logmsg;
+		}else{
+			$query = "SELECT * FROM tbl_admin WHERE email = '$adminUser' AND pass = '$adminPass'";
+			$result = $this->db->select($query);
+			if ($result !=false) {
+				$value = $result->fetch_assoc();
+				Session::set("login", true);
+				Session::set("adminId",   $value['id']);
+				Session::set("adminName", $value['nameame']);
+				// Session::set("adminUser", $value['adminUser']);
+				header("Location:index.php");
+			}else{
+				$logmsg = "Username Or Password Not Match!!";
+			    return $logmsg;
+			}
+		}
+	}
+}
+?>
