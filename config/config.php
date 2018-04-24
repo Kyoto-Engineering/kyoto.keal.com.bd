27f057fc8a98b5f8f70adc0352f50ad25f656f25
Protyasha-Saha <sahaprotyasha@gmail.com>
2018-04-24 12:03:13 +0600

    added

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
    <?php
 	    define("DB_HOST", "localhost");
-	    define("DB_USER", "kealcom"); #needs change on host
-	    define("DB_PASS", "Cought#2{Red&Handed}@Salehn"); 
-	    define("DB_NAME", "kealcom_training");
+	    define("DB_USER", "root"); #needs change on host
+	    define("DB_PASS", ""); 
+	    define("DB_NAME", "keal_training");
     ?>
